a48a3c6aa9c15b0d6821365788ba133b1d6e2d70
曾思信 <389585111@qq.com>
2025-02-25 00:15:01 +0800

    测试

--- TopClient/Assets/GameScript/HotUpdate/UIGen/Login/LoginBinder.cs
+++ TopClient/Assets/GameScript/HotUpdate/UIGen/Login/LoginBinder.cs
@@ -8,16 +8,10 @@ namespace Login
     {
         public static void BindAll()
         {
-            UIObjectFactory.SetPackageItemExtension(list_clothes_00.URL, typeof(list_clothes_00));
-            UIObjectFactory.SetPackageItemExtension(landItem1.URL, typeof(landItem1));
             UIObjectFactory.SetPackageItemExtension(LoginMainView.URL, typeof(LoginMainView));
             UIObjectFactory.SetPackageItemExtension(ComboBox1_popup.URL, typeof(ComboBox1_popup));
             UIObjectFactory.SetPackageItemExtension(ComboBox1_item.URL, typeof(ComboBox1_item));
-            UIObjectFactory.SetPackageItemExtension(clothes_btn_01.URL, typeof(clothes_btn_01));
-            UIObjectFactory.SetPackageItemExtension(btn_choose.URL, typeof(btn_choose));
             UIObjectFactory.SetPackageItemExtension(ServerListRemoteView.URL, typeof(ServerListRemoteView));
-            UIObjectFactory.SetPackageItemExtension(list_clothes_04.URL, typeof(list_clothes_04));
-            UIObjectFactory.SetPackageItemExtension(fold.URL, typeof(fold));
         }
     }
 }
